ada629cb610b9ec59d0d8ad3725be7fb625fb7ff
Jake Peterson <jake@popsql.com>
2023-12-05 18:16:31 +0000

    add returned time and month

--- models/staging/stg_ecommerce__orders.sql
+++ models/staging/stg_ecommerce__orders.sql
@@ -11,9 +11,11 @@ select
 	--order timestamps in US central time
 	{{ ntz_to_reporting_tz('created_at') }}  as order_created_at,
 	{{ ntz_to_reporting_tz('shipped_at') }}  as order_shipped_at,
+	{{ ntz_to_reporting_tz('returned_at') }}  as order_returned_at,
 
 	--order dates in US central time
 	{{ ntz_to_reporting_tz_month('created_at') }}  as order_created_month,
-	{{ ntz_to_reporting_tz_month('shipped_at') }}  as order_shipped_month
+	{{ ntz_to_reporting_tz_month('shipped_at') }}  as order_shipped_month,
+	{{ ntz_to_reporting_tz_month('returned_at') }}  as order_returned_month
 
 from {{ source('ecommerce','orders') }}
